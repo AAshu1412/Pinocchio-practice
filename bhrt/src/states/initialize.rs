b2cc0ada0c09f97ff665e76cc26db0d92c4c5104
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2025-09-16 10:22:09 +0530

    bhrt token initialization instruction

--- bhrt/src/states/initialize.rs
+++ bhrt/src/states/initialize.rs
@@ -1,6 +1,16 @@
-use pinocchio::{account_info::AccountInfo, instruction, program_error::ProgramError, pubkey::find_program_address, ProgramResult};
+use bytemuck::{Pod, Zeroable};
+use pinocchio::{
+    account_info::AccountInfo,
+    instruction::{Seed, Signer},
+    program_error::ProgramError,
+    pubkey::{find_program_address, Pubkey},
+    sysvars::rent::{Rent},
+};
+use pinocchio_system::instructions::CreateAccount;
+use pinocchio_token_2022::instructions::{InitializeMint, InitializeMint2};
+use pinocchio_token_2022::ID as TOKEN_PROGRAM_ID;
 
-pub struct InitializeAccounts<'a>{
+pub struct InitializeAccounts<'a> {
     pub authority: &'a AccountInfo,
     pub program_state: &'a AccountInfo,
     pub bhrt_mint: &'a AccountInfo,
@@ -16,33 +26,72 @@ pub struct InitializeAccounts<'a>{
     pub system_program: &'a AccountInfo,
 }
 
-impl <'a> TryFrom<&'a [AccountInfo]> for InitializeAccounts<'a> {
+#[repr(C)]
+#[derive(Pod, Zeroable, Clone, Copy, Debug, PartialEq, shank::ShankAccount)]
+pub struct ProgramState {
+    pub authority: Pubkey,
+    pub program_state_bump: u8,
+    pub bhrt_mint_bump: u8,
+    pub collection_mint_bump: u8,
+    pub collection_metadata_bump: u8,
+}
+
+impl<'a> TryFrom<&'a [AccountInfo]> for InitializeAccounts<'a> {
     type Error = ProgramError;
 
     fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
-        let [owner, vault, _] = accounts else {
-            return  Err(ProgramError::NotEnoughAccountKeys);
+        let [authority, program_state, bhrt_mint, instruction_sysvar, _] = accounts else {
+            return Err(ProgramError::NotEnoughAccountKeys);
         };
 
-        if !owner.is_signer() {
+        if !authority.is_signer() {
             return Err(ProgramError::MissingRequiredSignature);
         }
 
-        if !vault.is_owned_by(&pinocchio_system::ID) {
+        let rent = Rent::from_account_info(instruction_sysvar)?;
+
+        // derive the canonical bump during account init
+        let (derived_program_state_pda, bump) = find_program_address(&[b"program_state"], &crate::ID);
+        if derived_program_state_pda.ne(program_state.key()) {
             return Err(ProgramError::InvalidAccountOwner);
         }
+        //Signer Seeds
+        let program_state_signer_seeds = [Seed::from(b"program_state"), Seed::from(&[bump])];
+        let program_state_signers = [Signer::from(&program_state_signer_seeds[..])];
 
-        if vault.lamports().ne(&0) {
-            return  Err(ProgramError::InvalidAccountData);
+        CreateAccount {
+            from: authority,
+            to: program_state,
+            space: core::mem::size_of::<ProgramState>() as u64,
+            owner: &crate::ID,
+            lamports: rent.minimum_balance(core::mem::size_of::<ProgramState>()),
         }
+        .invoke_signed(&program_state_signers)?;
 
-        let (vault_key, _) = find_program_address(&[b"vault", owner.key()], &crate::ID);
+        InitializeMint2 {
+            mint: bhrt_mint,
+            decimals: 9,
+            mint_authority: program_state.key(),
+            freeze_authority: Some(program_state.key()),
+            token_program: &TOKEN_PROGRAM_ID,
+        }.invoke()?;
 
-        if vault.key().ne(&vault_key){
-            return Err(ProgramError::InvalidAccountOwner);
-        }
+        
+
+        // if !vault.is_owned_by(&pinocchio_system::ID) {
+        //     return Err(ProgramError::InvalidAccountOwner);
+        // }
+
+        // if vault.lamports().ne(&0) {
+        //     return  Err(ProgramError::InvalidAccountData);
+        // }
+
+        // let (vault_key, _) = find_program_address(&[b"vault", owner.key()], &crate::ID);
+
+        // if vault.key().ne(&vault_key){
+        //     return Err(ProgramError::InvalidAccountOwner);
+        // }
 
         Ok(Self { owner, vault })
-
     }
 }
